3bf182ade4b4452d761a75654d45a7fc44404ef5
sian7563 <sian7563@naver.com>
2021-12-12 02:23:57 +0900

    ⚡️ :: Change Password Button Custom & 화면전환

--- Unknown/Unknown/ViewController/MypageViewController.swift
+++ Unknown/Unknown/ViewController/MypageViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class MypageViewController: UIViewController {
     
+    @IBOutlet weak var changePWBtn: UIButton!
     @IBOutlet weak var profileImgView: UIImageView!
     
     override func viewDidLoad() {
@@ -16,6 +17,13 @@ class MypageViewController: UIViewController {
         
         let url = URL(string: "https://avatars.githubusercontent.com/u/81298254?v=4")
         profileImgView.load(url: url!)
+        changePWBtn.setTitle("비밀번호 변경", for: .normal)
+        let aa = NSMutableAttributedString(string: changePWBtn.currentTitle ?? "")
+        let underLine = NSUnderlineStyle.thick.rawValue
+        aa.addAttribute(NSMutableAttributedString.Key.underlineStyle,
+                        value: underLine,
+                        range: NSRange(location: 0, length: changePWBtn.currentTitle!.count))
+        changePWBtn.setAttributedTitle(aa, for: .normal)
         // Do any additional setup after loading the view.
     }
 
@@ -28,9 +36,12 @@ class MypageViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func changepwPush(_ sender: UIButton) {
+            let pushVC = self.storyboard?.instantiateViewController(withIdentifier: "changePW")
+            self.navigationController?.pushViewController(pushVC!, animated: true)
+        }
 
 }
-
 extension UIImageView {
     func load(url: URL) {
         DispatchQueue.global().async { [weak self] in
@@ -38,9 +49,9 @@ extension UIImageView {
                 if let image = UIImage(data: data) {       DispatchQueue.main.async {
                             self?.image = image
                         }
-                    }
                 }
             }
         }
     }
-
+}
+   
